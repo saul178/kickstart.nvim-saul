ba49808c9be360255ef0fb1468615f418df83c59
leviathan <gonzalezsaul31@gmail.com>
2024-08-27 10:25:39 -0600

    fixed an issue

--- init.lua
+++ init.lua
@@ -796,16 +796,16 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     -- 'rose-pine/neovim',
 
-    -- name = 'rose-pine',
     name = 'tokyonight',
+    -- name = 'rose-pine',
 
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'rose-pine'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
