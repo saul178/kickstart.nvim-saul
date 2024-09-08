f76d0548fcc932327e7372d3fc3daba89211bbad
Saul Gonzalez <gonzalezsaul31@gmail.com>
2024-09-07 19:16:24 -0600

    removed the file types field cause it caused issues

--- init.lua
+++ init.lua
@@ -606,9 +606,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
-          filetypes = { 'js', 'ts' },
-        },
+        tsserver = {},
 
         lua_ls = {
           -- cmd = {...},
