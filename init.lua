151d5ac44d5bada4efe9ceb3b3f107689ea3c08a
leviathan <gonzalezsaul31@gmail.com>
2025-01-21 09:30:53 -0700

    changed the color of the bar on the laptop

--- init.lua
+++ init.lua
@@ -749,6 +749,7 @@ require('lazy').setup({
       -- vim.cmd.colorscheme 'rose-pine'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.cmd 'highlight ColorColumn ctermbg=0 guibg=#4E737F'
     end,
   },
 
