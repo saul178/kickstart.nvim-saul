1517e0c23de027087e9467ab5cb6026e488856bb
leviathan <gonzalezsaul31@gmail.com>
2025-01-31 23:21:30 -0700

    done with the laptop fauna theme

--- init.lua
+++ init.lua
@@ -738,21 +738,25 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
     -- 'rose-pine/neovim',
-
-    name = 'tokyonight',
-
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'comfysage/evergarden',
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      transparent_background = true,
+      variant = 'hard', -- 'hard'|'medium'|'soft'
+      overrides = {}, -- add custom overrides
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
       -- vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'evergarden'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
-      vim.cmd 'highlight ColorColumn ctermbg=0 guibg=#4E737F'
+      vim.cmd 'highlight ColorColumn ctermbg=0 guibg=#B6D9AA'
     end,
   },
 
